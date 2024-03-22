5b0c2f7265b1059710d93e2c4c83b0972b7a763f
imyeji2 <137904402+imyeji2@users.noreply.github.com>
2024-03-22 15:56:12 +0900

    실습 1-7) 1부터 n까지 정수의 합(while문)

---
+++ src/Algorithm/chap01/SumWhile.java
@@ -0,0 +1,33 @@
+package Algorithm.chap01;
+
+import java.util.Scanner;
+
+public class SumWhile {
+	public static void main(String[]args) {
+		
+		//1부터 n까지 정수의 합 구하기
+		Scanner sdtIn = new Scanner(System.in);
+		
+		System.out.println("1부터 n까지의 합을 구합니다.");
+		System.out.print("n값:");
+		int n = sdtIn.nextInt();
+		
+		int sum =0; //합 저장 변수 초기화
+		int i=1; //시작값
+		
+		while(i<=n) { //1부터 n가지 반복
+			sum+=i; //sum에 i 더하기
+			i++; //i값 1씩 증가
+		}
+		
+		System.out.println("1부터 " + n +"까지의 합은 " +sum+" 입니다.");
+		
+		/* 반복문
+		 * 조건이 성립하는 동안 처리를 반복하여 실행하는 것, 루프라고 부름
+		 * 1. while문
+		 * while문은 실행 전 반복을 계속할지를 판단 -> 사전 판단 반복
+		 * while(제어식) 명령문
+		 */
+
+	}
+}
